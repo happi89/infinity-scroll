a5065f684ba03c443e1e6aaeebbf392af8ee606d
Farhaan Patel <101960666+happi89@users.noreply.github.com>
2022-05-27 10:12:52 -0400

    finished project v1

---
+++ script.js
@@ -0,0 +1,81 @@
+const imageContainer = document.getElementById('img-container');
+const loader = document.getElementById('loader')
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+// unsplash api
+const count = 30;
+const apiKey = '-lU3AJ9ouM_EaUB8FzuLw_YrnPXBO0_fwmyndrTTKDA';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// image loaded function
+function imageLoaded() {
+	imagesLoaded++
+	if (imagesLoaded === totalImages) {
+		ready = true;
+		loader.hidden = true;
+	}
+}
+
+
+// set attributes function
+function setAttributes(element, attributes) {
+	for (const key in attributes) {
+		element.setAttribute(key, attributes[key])
+	}
+}
+
+
+// create elements 
+
+function displayImages() {
+	imagesLoaded = 0;
+	totalImages = photosArray.length;
+	photosArray.forEach((photo) => {
+		const link = document.createElement('a');
+		setAttributes(link, 
+			{
+				href: photo.links.html,
+				target: '_blank'
+			});
+
+		const img = document.createElement('img');
+		setAttributes(img, {
+			src: photo.urls.regular,
+			alt: photo.alt_description,
+			title: photo.alt_description
+		});
+
+		img.addEventListener('load', imageLoaded);
+
+		// append
+		link.appendChild(img);
+		imageContainer.appendChild(link);
+	});
+}
+
+// get photos
+async function getImages() {
+	try {
+		const response = await fetch(apiUrl)
+		photosArray = await response.json();
+		displayImages();
+	} catch(error) {
+		// catch error here
+	}
+}
+
+// check to see if scrolling near bottom of page, load more image
+window.addEventListener('scroll', () => {
+	if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready) {
+		ready = false;
+		getImages();
+	}
+});
+
+// on load
+getImages();
